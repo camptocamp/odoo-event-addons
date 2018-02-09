9537faabd4cfeae5c65da113a91787093ab6d6a3
Timon Tschanz <timon.tschanz@camptocamp.com>
2018-02-09 15:05:50 +0100

    Add event_firstname module

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # odoo-event-addons
 Our collection of Odoo events management addons
+
+## event_registration_firstname
+
+Adds related fields firstname and lastname and makes name a related field of the partner_id.
---
+++ event_registration_firstname/models/event_registration.py
@@ -0,0 +1,27 @@
+from odoo import models, fields
+
+
+class EventRegistration(models.Model):
+
+    _inherit = "event.registration"
+
+    firstname = fields.Char(
+                            string="Firstname",
+                            related="partner_id.firstname",
+                            readonly=True,
+                            store=True
+                           )
+
+    lastname = fields.Char(
+                           string="Lastname",
+                           related="partner_id.lastname",
+                           readonly=True,
+                           store=True
+                          )
+
+    name = fields.Char(
+                       string="Name",
+                       related="partner_id.name",
+                       readonly=True,
+                       store=True
+                      )
