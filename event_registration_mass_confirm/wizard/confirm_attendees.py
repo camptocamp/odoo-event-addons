a9f5fcab15f3246ca7d4c10a3c7b8f0a9b79e45e
Timon Tschanz <timon.tschanz@camptocamp.com>
2018-03-19 15:25:45 +0100

    requested changes

--- event_registration_mass_confirm/wizard/confirm_attendees.py
+++ event_registration_mass_confirm/wizard/confirm_attendees.py
@@ -1,6 +1,7 @@
 # Copyright 2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import models, api, _
+from odoo.exceptions import UserError
 
 
 class EventRegistrationMassConfirm(models.TransientModel):
@@ -10,20 +11,23 @@ class EventRegistrationMassConfirm(models.TransientModel):
     @api.multi
     def confirm_registrations(self):
         """Validate selected registrations."""
-        context = dict(self.env.context or {})
-        active_ids = context.get('active_ids', []) or []
-
+        context = self.env.context or {}
+        active_ids = context.get('active_ids') or []
+        if not active_ids:
+            raise UserError(_("Please select a Registration to confirm."))
         registrations = self.env['event.registration'].browse(active_ids)
 
-        if len(registrations) > 0:
+        if registrations:
             registrations.confirm_registration()
-
-        return {'domain': "[('id', 'in', %s)]" % registrations.ids,
+        return {'domain': "[('id', 'in', {0})]".format(registrations.ids),
                 'name': _("Mass confirmed registrations"),
                 'auto_search': True,
                 'res_model': 'event.registration',
-                'views': [
-                    (self.env.ref('event.view_event_registration_tree').id, 'tree')],
+                'context': context,
+                'views': [(
+                    self.env.ref('event.view_event_registration_tree').id,
+                    'tree'
+                )],
                 'search_view_id': False,
                 'type': 'ir.actions.act_window'
                 }
