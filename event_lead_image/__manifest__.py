f15355b1bdd9f2e3d7b6f9fa51db01ea6f3b7894
Simone Orsi <simahawk@gmail.com>
2018-03-23 10:04:48 +0100

    event_lead_image: bump 11.0.1.1.0

--- event_lead_image/__manifest__.py
+++ event_lead_image/__manifest__.py
@@ -1,6 +1,6 @@
 {'name': 'Event Leadimage',
  'summary': "Adds a Lead image for events.",
- 'version': '11.0.1.0.0',
+ 'version': '11.0.1.1.0',
  'author': 'Camptocamp, Odoo Community Association (OCA)',
  'license': 'AGPL-3',
  'category': 'Others',
