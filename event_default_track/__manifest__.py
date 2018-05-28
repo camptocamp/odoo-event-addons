2acce3d8b86b661baa63c13ed9ecfda27db2750a
Simone Orsi <simahawk@gmail.com>
2018-05-28 11:36:11 +0200

    Bump versions

--- event_default_track/__manifest__.py
+++ event_default_track/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Event Default Track',
     'summary': "Automatically create a default track for events.",
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.0.1',
     'author': 'Camptocamp, Odoo Community Association (OCA)',
     'license': 'AGPL-3',
     'category': 'Others',
--- event_lead_image/__manifest__.py
+++ event_lead_image/__manifest__.py
@@ -1,6 +1,6 @@
 {'name': 'Event Leadimage',
  'summary': "Adds a Lead image for events.",
- 'version': '11.0.1.1.1',
+ 'version': '11.0.1.1.2',
  'author': 'Camptocamp, Odoo Community Association (OCA)',
  'license': 'AGPL-3',
  'category': 'Others',
