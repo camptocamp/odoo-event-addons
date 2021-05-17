edf07c0b6cea377bc268cd754366bc6c2d2671bc
Simone Orsi <simahawk@gmail.com>
2021-05-17 09:51:32 +0200

    Add event_tracks_by_day

---
+++ event_tracks_by_day/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2018 Camptocamp SA
+# @author Simone Orsi <simahawk@gmail.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    "name": "Event tracks by day",
+    "summary": "Shared module to group tracks by date",
+    "version": "13.0.1.0.0",
+    "author": "Camptocamp",
+    "license": "LGPL-3",
+    "category": "Others",
+    "depends": ["event"],
+    "website": "https://github.com/camptocamp/odoo-event-addons",
+    "data": [],
+    "installable": True,
+}
