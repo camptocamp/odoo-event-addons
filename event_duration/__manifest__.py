08d139801ced7963c1c72a5c07711d146e9396d3
Simone Orsi <simahawk@gmail.com>
2021-05-18 14:10:25 +0200

    Add event_duration

---
+++ event_duration/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2018 Camptocamp SA
+# @author Simone Orsi <simahawk@gmail.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    "name": "Event duration utils",
+    "summary": "Shared module to ease dealing w/ event duration",
+    "version": "13.0.1.0.0",
+    "author": "Camptocamp",
+    "license": "LGPL-3",
+    "category": "Others",
+    "depends": ["event"],
+    "website": "https://github.com/camptocamp/odoo-event-addons",
+    "data": [],
+    "installable": True,
+}
