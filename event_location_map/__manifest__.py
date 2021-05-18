0937c5b1c3a41c75b55ccf81217a9381b662a0c2
Simone Orsi <simahawk@gmail.com>
2021-05-18 14:39:56 +0200

    Add event_location_map + badge integration

---
+++ event_location_map/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2018 Camptocamp SA
+# @author Simone Orsi <simahawk@gmail.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    "name": "Event location map",
+    "summary": "Add location map image",
+    "version": "13.0.1.0.0",
+    "author": "Camptocamp",
+    "license": "LGPL-3",
+    "category": "Others",
+    "depends": ["event"],
+    "website": "https://github.com/camptocamp/odoo-event-addons",
+    "data": ["views/res_partner_view.xml"],
+    "installable": True,
+}
